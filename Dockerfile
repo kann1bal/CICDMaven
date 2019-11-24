4a12f3351c95c7d5f43d74f123e48fe52c7674a0
kann1bal <mohamedzied.barketallah@esprit.tn>
2019-11-24 13:28:40 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk:8-jdk
+MAINTAINER Daniel Persson (mohamedzied.barketallah@esprit.tn)
+RUN apt-get update
+RUN apt-get install -y maven
+COPY pom.xml /usr/local/service/pom.xml
+COPY src /usr/local/service/src
+WORKDIR /usr/local/service
+RUN mvn package
+CMD ["java","-jar","target/docker-service-1.0-SNAPSHOT-jar-with-dependencies.jar"]
---
+++ src/main/java/tn/esprit/arctic/reservationservice/ReservationServiceApplication.java
@@ -0,0 +1,16 @@
+package tn.esprit.arctic.reservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationServiceApplication.class, args);
+		System.out.print("ahhh ahhh ahhh staying alive 5 arctic 1");
+
+
+	}
+
+}
